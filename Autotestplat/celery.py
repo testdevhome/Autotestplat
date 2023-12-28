5d4cb624e6eff5f6642f7dcd4da6c5c10f4a0221
Fin <37730772+testdevhome@users.noreply.github.com>
2023-12-28 11:47:43 +0800

    Autotestplat-V5.0

--- Autotestplat/celery.py
+++ Autotestplat/celery.py
@@ -17,6 +17,9 @@ app = Celery('Autotestplat')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+app.conf.enable_utc = True
+app.conf.timezone = "Asia/Shanghai"
+
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
