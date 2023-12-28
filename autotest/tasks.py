8d4ae4bd3729be9cd3aecf8ca48719dab3b4d3ae
Fin <37730772+testdevhome@users.noreply.github.com>
2023-12-28 13:20:05 +0800

    Autotestplat-V5.0

--- autotest/tasks.py
+++ autotest/tasks.py
@@ -1,11 +1,6 @@
 ﻿# -*- coding:utf-8 -*-
 from Autotestplat.celery import app
 
-
-@app.task
-def hello_world():
-    print('已运行')
-
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
